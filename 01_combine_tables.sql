4a2c256999b1d8bb1df52ee282db777c76517a5b
Tejaswini Samanta <171205931+tejaswinisamanta@users.noreply.github.com>
2026-02-07 17:24:17 +0530

    Create 01_combine_tables.sql

---
+++ 01_combine_tables.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE `bigquery-486416.ny_taxi.yellow_taxi_2024`
+AS
+SELECT * FROM `bigquery-486416.ny_taxi.NYC Taxi Jan`
+UNION ALL
+SELECT * FROM `bigquery-486416.ny_taxi.NYC Taxi Feb`
+UNION ALL
+SELECT * FROM `bigquery-486416.ny_taxi.NYC Taxi March`
+UNION ALL
+SELECT * FROM `bigquery-486416.ny_taxi.NYC Taxi Apr`
+UNION ALL
+SELECT * FROM `bigquery-486416.ny_taxi.NYC Taxi May`
+UNION ALL
+SELECT * FROM `bigquery-486416.ny_taxi.NYC Taxi June`;
